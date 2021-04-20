cf9d467f9f6c5031a906797276b93e2676ccee0c
Your Name <you@example.com>
2021-04-20 15:43:50 +0300

    post model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    link = models.CharField(max_length=2048)
+    author_name = models.CharField(max_length=255, null=False)
+    upvoted = models.IntegerField(default=0)
+    created = models.DateField(editable=False, auto_now_add=True)
