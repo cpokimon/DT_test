e752cb4e88a3f461626a23aafedf18f1ad9be443
unknown <f.f.bondar@gmail..com>
2021-04-20 16:49:41 +0300

    comment added

--- news/models.py
+++ news/models.py
@@ -4,8 +4,16 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255, null=False)
+    title = models.CharField(max_length=255, null=False, blank=False)
     link = models.CharField(max_length=2048)
-    author_name = models.CharField(max_length=255, null=False)
+    author_name = models.CharField(max_length=255, null=False, blank=False)
     upvoted = models.IntegerField(default=0)
     created = models.DateField(editable=False, auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    authon_name = models.CharField(max_length=255, null=False, blank=False)
+    content = models.TextField()
+    created = models.DateField(editable=False, auto_now_add=True)
+
